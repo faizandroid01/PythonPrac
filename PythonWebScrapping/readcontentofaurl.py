f3a2c1e1d2b73443ee9bc19cb9039c2a4ae5c015
faiz-pitavya <faizrocks9211@gmail.com>
2020-05-16 21:12:15 +0530

    First Commit from faiz # Added DataStructure Concept Folder added

--- PythonWebScrapping/readcontentofaurl.py
+++ PythonWebScrapping/readcontentofaurl.py
@@ -1,6 +1,18 @@
 import requests as req
+from bs4 import BeautifulSoup as b
 
 r = req.get("https://www.google.com")
 
 c = r.content
-print(c)
+# print(c)
+
+# get the Beautiful soup constructor
+soup = b(c, "html.parser")
+
+# prettify the xml content
+print(soup.prettify())
+
+print('------------------------------------------------------------------------------------')
+# get all the link from the xml
+for links in soup.findAll("a"):
+    print(links.get("href"))
